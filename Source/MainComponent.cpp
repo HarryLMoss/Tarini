4a0967bdd70a40f69914d7bbe11c42c09cadcb0a
Harry Moss <harrymoss33@gmail.com>
2026-04-28 15:52:57 +0100

    Update MainComponent.cpp

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,5 +1,7 @@
-# Author: Harry Moss
-# Date: 29.05.2024
+// ==========================================================
+// Author: Harry Moss
+// Date: 29.05.2024
+// ==========================================================
 
 #include "MainComponent.h"
 
@@ -7,28 +9,46 @@ MainComponent::MainComponent()
 {
     setSize(600, 300);
 
+    // Tonic control
+
     tonicSlider.setRange(110.0, 440.0, 1.0);
     tonicSlider.setValue(146.83);
+
     tonicSlider.addListener(this);
-    tonicSlider.setTextBoxStyle(juce::Slider::TextBoxBelow,
-                                false,
-                                80,
-                                20);
+
+    tonicSlider.setTextBoxStyle(
+        juce::Slider::TextBoxBelow,
+        false,
+        80,
+        20);
+
     addAndMakeVisible(tonicSlider);
 
-    tonicLabel.setText("Tonic Frequency", juce::dontSendNotification);
+    tonicLabel.setText(
+        "Tonic Frequency",
+        juce::dontSendNotification);
+
     addAndMakeVisible(tonicLabel);
+
+    // Master gain control
 
     gainSlider.setRange(0.0, 1.0, 0.01);
     gainSlider.setValue(0.3);
+
     gainSlider.addListener(this);
-    gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow,
-                               false,
-                               80,
-                               20);
+
+    gainSlider.setTextBoxStyle(
+        juce::Slider::TextBoxBelow,
+        false,
+        80,
+        20);
+
     addAndMakeVisible(gainSlider);
 
-    gainLabel.setText("Master Gain", juce::dontSendNotification);
+    gainLabel.setText(
+        "Master Gain",
+        juce::dontSendNotification);
+
     addAndMakeVisible(gainLabel);
 
     setAudioChannels(0, 2);
@@ -57,9 +77,14 @@ void MainComponent::updateFrequencies()
 {
     float tonic = (float) tonicSlider.getValue();
 
+    // Tanpura-inspired tuning
+
     string1.setFrequency(tonic);
+
     string2.setFrequency(tonic * 1.5f);
+
     string3.setFrequency(tonic * 2.0f);
+
     string4.setFrequency(tonic * 2.0f * 1.01f);
 
     string1.setGain(0.20f);
@@ -68,20 +93,29 @@ void MainComponent::updateFrequencies()
     string4.setGain(0.15f);
 }
 
-void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+void MainComponent::getNextAudioBlock(
+    const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    auto* leftBuffer = bufferToFill.buffer->getWritePointer(0,
-                                                            bufferToFill.startSample);
+    auto* leftBuffer =
+        bufferToFill.buffer->getWritePointer(
+            0,
+            bufferToFill.startSample);
 
-    auto* rightBuffer = bufferToFill.buffer->getWritePointer(1,
-                                                             bufferToFill.startSample);
+    auto* rightBuffer =
+        bufferToFill.buffer->getWritePointer(
+            1,
+            bufferToFill.startSample);
 
-    for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
+    for (int sample = 0;
+         sample < bufferToFill.numSamples;
+         ++sample)
     {
         float s1 = string1.process();
         float s2 = string2.process();
         float s3 = string3.process();
         float s4 = string4.process();
+
+        // Stereo field distribution
 
         float left =
             (s1 * 0.8f) +
@@ -103,7 +137,8 @@ void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& buffer
     }
 }
 
-void MainComponent::sliderValueChanged(juce::Slider* slider)
+void MainComponent::sliderValueChanged(
+    juce::Slider* slider)
 {
     if (slider == &tonicSlider)
     {
@@ -112,7 +147,8 @@ void MainComponent::sliderValueChanged(juce::Slider* slider)
 
     if (slider == &gainSlider)
     {
-        masterGain = (float) gainSlider.getValue();
+        masterGain =
+            (float) gainSlider.getValue();
     }
 }
 
@@ -121,21 +157,25 @@ void MainComponent::paint(juce::Graphics& g)
     g.fillAll(juce::Colours::black);
 
     g.setColour(juce::Colours::orange);
+
     g.setFont(28.0f);
 
-    g.drawFittedText("Tarini",
-                     getLocalBounds().removeFromTop(60),
-                     juce::Justification::centred,
-                     1);
+    g.drawFittedText(
+        "Tarini",
+        getLocalBounds().removeFromTop(60),
+        juce::Justification::centred,
+        1);
 
     g.setFont(16.0f);
-    g.drawFittedText("Tanpura-inspired Generative Drone Engine",
-                     0,
-                     50,
-                     getWidth(),
-                     30,
-                     juce::Justification::centred,
-                     1);
+
+    g.drawFittedText(
+        "Tanpura-inspired Generative Drone Engine",
+        0,
+        50,
+        getWidth(),
+        30,
+        juce::Justification::centred,
+        1);
 }
 
 void MainComponent::resized()
